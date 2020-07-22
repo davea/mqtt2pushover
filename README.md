6a57f9c5e1a8e1f806bc2c0c790707f1ca4b6ac3
Dave Arter <github@davea.me>
2020-07-22 21:51:48 +0100

    Add Ansible role & code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 A simple script to trigger [Pushover](https://pushover.net/) messages from MQTT,
 using [mqttwrapper](https://github.com/davea/mqttwrapper).
+
+This directory is intended to be used as an Ansible role - the actual code is in
+`files/mqtt2pushover.py`.
---
+++ files/mqtt2pushover.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import json
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from mqttwrapper import run_script
+
+
+def callback(topic, payload, config):
+    config = config[topic]
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        {
+            "token": config["app_key"],
+            "user": config["user_key"],
+            "message": config["message"],
+            "sound": config.get("sound"),
+            "title": config.get("title"),
+        },
+        timeout=5,
+    )
+
+
+def main():
+    config = json.loads(os.environ["MQTT_TOPICS_CONFIG"])
+    run_script(callback, topics=config.keys(), config=config)
+
+
+if __name__ == "__main__":
+    main()
